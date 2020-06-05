36e47db5df6d3b07fa2f9011325a91d98e537ff8
anakettl <anakettl@gmail.com>
2020-06-05 01:40:32 -0300

    Create new routes to post /users and get users/:id

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,48 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
+// Rota: endereco completo da requisicao
+// Recurso: qual entidade estamos acessando no sistema
+
+// GET: Pegar uma ou mais informacoes do back-end
+// POST: Criar uma nova informacao no back-end
+// PUT: Atualizar uma informacao existente no back end
+// DELETE: Remover uma informacao do back-end
+
+// POST: http://localhost:3333/users = criar um usuario
+// GET: http://localhost:3333/users = pegar informacoes de um usuario
+// GET: http://localhost:3333/users/5 = pegar informacoes do usuario com id 5
+
+ const users = [
+    'Ana',
+    'Paulo',
+    'Maiara'
+]
+
 app.get('/users', (request, response) => {
-    response.json([
-        'Ana',
-        'Gato',
-        'Mala',
-        'Abacaxi',
-        'Feira',
-        'Organica',
-        'Pare',
-        'Ja'
-    ]);
+    // Listar usuarios
+    return response.json(users);
+})
+
+app.get('/users/:id', (request, response) => {
+    // listar um usuario
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+})
+
+app.post('/users', (request, response) => {
+    // criar um usuario
+
+    const user = {
+        nome: 'Felipe',
+        email: 'felipe@email.com'
+    }
+
+    return response.json(user);
 })
 
 app.listen(3333);
