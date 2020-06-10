7f9c69085b832575adf1ddd3b95e8bfa2daf9447
anakettl <anakettl@gmail.com>
2020-06-09 23:35:32 -0300

    Add new route post to insert point

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,19 +1,61 @@
-import express from 'express';
+import express, { request, response } from 'express';
 import knex from './database/connection';
 
 const routes = express.Router();
 
 routes.get('/items', async(request, response) => {
-    const items = await knex('items').select('*');
+  const items = await knex('items').select('*');
 
-    const serializedItems = items.map(item => {
-        return {
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`,
-        }
-    })
+  const serializedItems = items.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    }
+  })
 
-    return response.json(serializedItems);
+  return response.json(serializedItems);
 })
+
+routes.post('/points', async(request, response) => {
+  //desestruturacao, é igual a const name = request.body.name
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = request.body;
+
+  //para interromper os scripts caso ocorra um erro de execucao
+  const trx = await knex.transaction();
+  // short sintaxe, nome da variavel é igual ao nome da propriedade do objeto
+  const insertedIds = await trx('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: Number) => {
+    return {
+      item_id,
+      point_id
+    };
+  });
+
+  await trx('point_items').insert(pointItems);
+
+  return response.json({ success: true});
+});
 
 export default routes;
