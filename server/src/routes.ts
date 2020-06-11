928965c405d88b019c588fb3e351c434708c5e56
anakettl <anakettl@gmail.com>
2020-06-10 22:43:08 -0300

    Tranfer create function to Points Controller

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import knex from './database/connection';
 
+import PointsController from './controllers/PointsController'
+
 const routes = express.Router();
+const pointsController = new PointsController();
 
 routes.get('/items', async(request, response) => {
   const items = await knex('items').select('*');
@@ -17,45 +20,6 @@ routes.get('/items', async(request, response) => {
   return response.json(serializedItems);
 })
 
-routes.post('/points', async(request, response) => {
-  //desestruturacao, é igual a const name = request.body.name
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items
-  } = request.body;
-
-  //para interromper os scripts caso ocorra um erro de execucao
-  const trx = await knex.transaction();
-  // short sintaxe, nome da variavel é igual ao nome da propriedade do objeto
-  const insertedIds = await trx('points').insert({
-    image: 'image-fake',
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf
-  });
-
-  const point_id = insertedIds[0];
-
-  const pointItems = items.map((item_id: number) => {
-    return {
-      item_id,
-      point_id,
-    };
-  });
-
-  await trx('point_items').insert(pointItems);
-
-  return response.json({ success: true});
-});
+routes.post('/points', pointsController.create);
 
 export default routes;
