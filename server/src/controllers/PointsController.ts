e9a073c94213fe22c5c1f4d9ec526e0f1e95a5b2
anakettl <anakettl@gmail.com>
2020-06-11 00:07:31 -0300

    Create new function to list all points with filter by city, uf and items

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -3,6 +3,24 @@ import knex from '../database/connection';
 
 class PointsController {
 
+async index (request: Request, response: Response) {
+  const { city, uf, items } = request.query;
+
+  const parsedItems = String(items)
+  .split(',')
+  .map(item => Number(item.trim()));
+
+  const points = await knex('points')
+  .join('point_items', 'points.id', '=', 'point_items.point_id')
+  .whereIn('point_items.item_id', parsedItems)
+  .where('city', String(city))
+  .where('uf', String(uf))
+  .distinct()
+  .select('points.*');
+
+  return response.json({ points });
+}
+
 async show (request: Request, response: Response) {
   //const id = request.params.id
   const { id } = request.params;
@@ -60,6 +78,8 @@ async create (request: Request, response: Response) {
   });
 
   await trx('point_items').insert(pointItems);
+
+  await trx.commit();
 
   return response.json({ 
     id: point_id,
