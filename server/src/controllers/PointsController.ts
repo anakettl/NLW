9938bde397b4c3aa30f9561c588642591d871c38
anakettl <anakettl@gmail.com>
2020-06-10 23:20:29 -0300

    Create new funcion to show one point

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -3,51 +3,64 @@ import knex from '../database/connection';
 
 class PointsController {
 
-    async create (request: Request, response: Response) {
-        //desestruturacao, é igual a const name = request.body.name
-        const {
-          name,
-          email,
-          whatsapp,
-          latitude,
-          longitude,
-          city,
-          uf,
-          items
-        } = request.body;
-      
-        //para interromper os scripts caso ocorra um erro de execucao
-        const trx = await knex.transaction();
+async show (request: Request, response: Response) {
+  //const id = request.params.id
+  const { id } = request.params;
 
-        const point = {
-            image: 'image-fake',
-            name,
-            email,
-            whatsapp,
-            latitude,
-            longitude,
-            city,
-            uf
-          }
+  const point = await knex('points').where('id', id).first();
 
-        const insertedIds = await trx('points').insert(point);
-      
-        const point_id = insertedIds[0];
-      
-        const pointItems = items.map((item_id: number) => {
-          return {
-            item_id,
-            point_id,
-          };
-        });
-      
-        await trx('point_items').insert(pointItems);
-      
-        return response.json({ 
-            id: point_id,
-            ...point,
-        });
-      }
+  if(!point) {
+    return response.status(400).json({ message: 'Point not found' });
+  }
+
+  return response.json(point);
+}
+
+async create (request: Request, response: Response) {
+  //desestruturacao, é igual a const name = request.body.name
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = request.body;
+  
+  //para interromper os scripts caso ocorra um erro de execucao
+  const trx = await knex.transaction();
+
+  const point = {
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  }
+
+  const insertedIds = await trx('points').insert(point);
+
+  const point_id = insertedIds[0];
+  
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id,
+    };
+  });
+
+  await trx('point_items').insert(pointItems);
+
+  return response.json({ 
+    id: point_id,
+    ...point,
+  });
+  }
 
 }
 
